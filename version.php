9cff1ef26cf2c92089242bba5366faa1ab2bdc22
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-08-19 04:40:10 +0100

    fix: bump version for db/access.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_richtext';
 $plugin->release = '0.0.1';
-$plugin->version = 2026081000;
+$plugin->version = 2026081900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 502];
